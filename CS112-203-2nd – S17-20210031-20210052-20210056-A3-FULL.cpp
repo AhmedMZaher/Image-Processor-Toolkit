de9b8a8c12307d251c0b18310a00bb4bd4ca224b
Ahmed Zaher <66391501+AhmedZaher5788@users.noreply.github.com>
2022-04-07 17:33:52 +0200

    Some edits
    Make it clean code, added menu

--- CS112-203-2nd – S17-20210031-20210052-20210056-A3-FULL.cpp
+++ CS112-203-2nd – S17-20210031-20210052-20210056-A3-FULL.cpp
@@ -1,5 +1,5 @@
 // FCI – Programming 1 – 2022 - Assignment 3
-// Program Name: xxxxxx.cpp
+// Program Name: CS112-203-2nd – S17-20210031-20210052-20210056-A3-FULL.cpp
 // Last Modification Date: 07/04/2022
 // Author1 and ID and Group: 20210031
 // Author2 and ID and Group: 20210052
@@ -7,70 +7,106 @@
 // Teaching Assistant: Eng/ Hagar
 // Purpose: For 6 filters in photoshop project
 
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include "bmplib.cpp"
+
+using namespace std;
+unsigned char image1[SIZE][SIZE];
+unsigned char image2[SIZE][SIZE];
+unsigned char result[SIZE][SIZE];
 
 
-void invertImage(){
-    for(int i=0; i<SIZE; i++){
-        for (int j = 0; j <SIZE ; j++) {
-            image[i][j]= 255-image[i][j];
+void loadImage(unsigned char arr[SIZE][SIZE]) {
+    char imageFileName[100];
+
+    cout << "Please Enter name of input image: " << endl;
+    cin >> imageFileName;
+
+    strcat(imageFileName, ".bmp");
+    readGSBMP(imageFileName, arr);
+}
+
+void saveImage(unsigned char res_arr[SIZE][SIZE]) {
+    char imageFileName[100];
+
+    cout << "Please Enter name of output image: " << endl;
+    cin >> imageFileName;
+
+    strcat(imageFileName, ".bmp");
+    writeGSBMP(imageFileName, res_arr);
+}
+
+void invertImage() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            result[i][j] = 255 - image1[i][j];
         }
     }
 }
+
 //---------------------------------
 void rotateImage() {
 
     int choice;
     cout << "Enter rotate value 90, 180 or 270: ";
     cin >> choice;
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            result[i][j] = image1[i][j];
+        }
+    }
     if (choice == 270) {
         for (int x = 1; x <= 3; x++) {
             for (int i = 0; i < SIZE; i++) {        //Transpose the image(rows to columns)
                 for (int j = i; j < SIZE; j++) {
-                    int ele = image[i][j];
-                    image[i][j] = image[j][i];
-                    image[j][i] = ele;
+                    int ele = result[i][j];
+                    result[i][j] = result[j][i];
+                    result[j][i] = ele;
                 }
             }
             for (int i = 0; i < SIZE; i++) {      // Flip transpose  horizonatally
                 for (int j = 0; j < SIZE / 2; j++) {
-                    int ele = image[i][j];
-                    image[i][j] = image[i][SIZE - 1 - j];
-                    image[i][SIZE - 1 - j] = ele;
+                    int ele = result[i][j];
+                    result[i][j] = result[i][SIZE - 1 - j];
+                    result[i][SIZE - 1 - j] = ele;
                 }
             }
         }
     }
-     else if (choice == 90) {   //rotate image by 90 deg
+    else if (choice == 90) {   //rotate image by 90 deg
         for(int i=0 ; i<SIZE;i++){        //Transpose the image(rows to columns)
             for(int j=i ;j < SIZE; j++){
-                int ele=image[i][j];
-                image[i][j]=image[j][i];
-                image[j][i]=ele;
+                int ele=result[i][j];
+                result[i][j]=result[j][i];
+                result[j][i]=ele;
             }
         }
         for(int i=0; i<SIZE; i++){      // Flip transpose  horizonatally
             for(int j=0; j<SIZE/2; j++){
-                int ele = image[i][j];
-                image[i][j]=image[i][SIZE-1-j];
-                image[i][SIZE-1-j]=ele;
+                int ele = result[i][j];
+                result[i][j]=result[i][SIZE-1-j];
+                result[i][SIZE-1-j]=ele;
             }
         }
-            }
+    }
 
     else if (choice == 180) {
         for (int x = 1; x <=2; x++) {
             for(int i=0 ; i<SIZE;i++){        //Transpose the image(rows to columns)
                 for(int j=i ;j < SIZE; j++){
-                    int ele=image[i][j];
-                    image[i][j]=image[j][i];
-                    image[j][i]=ele;
+                    int ele=result[i][j];
+                    result[i][j]=result[j][i];
+                    result[j][i]=ele;
                 }
             }
             for(int i=0; i<SIZE; i++){      // Flip transpose  horizonatally
                 for(int j=0; j<SIZE/2; j++){
-                    int ele = image[i][j];
-                    image[i][j]=image[i][SIZE-1-j];
-                    image[i][SIZE-1-j]=ele;
+                    int ele = result[i][j];
+                    result[i][j]=result[i][SIZE-1-j];
+                    result[i][SIZE-1-j]=ele;
                 }
             }
         }
@@ -78,69 +114,112 @@ void rotateImage() {
 }
 
 
-
-
-
-void Merge_Filter()
-{
-    LoadImage();
-    char imageFileName2[100];
-    cout << "Please enter name of image file to merge with:" << endl;
-    cin>>imageFileName2;
-    strcat(imageFileName2, ".bmp");
-    readGSBMP(imageFileName2, image2);
+void Merge_Filter() {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            res[i][j] = (image[i][j] + image2[i][j]) / 2;
+            result[i][j] = (image1[i][j] + image2[i][j]) / 2;
         }
     }
 }
-void Darken() {
+
+void darkenImage() {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            res[i][j] = image[i][j] / 2;
+            result[i][j] = image1[i][j] / 2;
         }
-
-    }
-}
-void Lighten() {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            res[i][j]=min(image[i][j]+50,255);
-        }
-
     }
 }
 
-void BlackWhiteImage(){
+void lightenImage() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            result[i][j] = min(image1[i][j] + 50, 255);
+        }
+    }
+}
+
+void BlackWhiteImage() {
     long long avg = 0;
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            avg += image[i][j];
+            avg += image1[i][j];
         }
     }
     avg /= (256 * 256);
     for (int i = 0; i < SIZE; ++i) {
         for (int j = 0; j < SIZE; ++j) {
-            if (image[i][j] > avg)
-                image[i][j] = 255;
+            if (image1[i][j] > avg)
+                result[i][j] = 255;
             else
-                image[i][j] = 0;
+                result[i][j] = 0;
         }
     }
 }
 
-void flipImageHorizontally(){
+void flipImageHorizontally() {
     for (int i = 0; i < SIZE; ++i) {
         for (int j = 0; j < SIZE; ++j) {
-            outputImage[i][SIZE - j - 1] = image[i][j];
+            result[i][SIZE - j - 1] = image1[i][j];
         }
     }
 }
-void flipImageVertically(){
+
+void flipImageVertically() {
     for (int i = 0; i < SIZE; ++i) {
         for (int j = 0; j < SIZE; ++j) {
-            outputImage[SIZE - i - 1][j] = image[i][j];
+            result[SIZE - i - 1][j] = image1[i][j];
         }
+    }
+}
+
+
+int main() {
+    int choice = 0;
+    while (choice != -1) {
+        cout << "Ahlan ya user ya habibi\n";
+        cout << "1- Black & White Filter\n";
+        cout << "2- Invert Filter\n";
+        cout << "3- Merge Filter\n";
+        cout << "4- Flip Image\n";
+        cout << "5- Darken and Lighten Image\n";
+        cout << "6- Rotate Image\n";
+        cin >> choice;
+        loadImage(image1);
+        if (choice == 1) {
+            BlackWhiteImage();
+        } else if (choice == 2) {
+            invertImage();
+        } else if (choice == 3) {
+            loadImage(image2);
+            Merge_Filter();
+        } else if (choice == 4) {
+            char choice;
+            cout << " Flip (h)orizontally or (v)ertically ?" << endl;
+            cin >> choice;
+            if (choice == 'h')
+                flipImageHorizontally();
+            else if (choice == 'v')
+                flipImageVertically();
+            else {
+                cout << "Invalid input\n";
+                continue;
+            }
+        } else if (choice == 5) {
+            char choice;
+            cout << " Do you want to (d)arken or (l)ighten?" << endl;
+            cin >> choice;
+            if (choice == 'd')
+                darkenImage();
+            else if (choice == 'l')
+                lightenImage();
+            else {
+                cout << "Invalid input\n";
+                continue;
+            }
+
+        } else if (choice == 6) {
+            rotateImage();
+        }
+        saveImage(result);
     }
 }
