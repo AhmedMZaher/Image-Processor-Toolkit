be9921a1994fde601cef859d258ce1f2e8254bb2
Ahmed Zaher <66391501+AhmedZaher5788@users.noreply.github.com>
2022-04-23 21:38:07 +0200

    add header

--- CS112-203-2nd – S17-20210031-20210052-20210056-A3-Bouns.cpp
+++ CS112-203-2nd – S17-20210031-20210052-20210056-A3-Bouns.cpp
@@ -1,3 +1,11 @@
+// FCI – Programming 1 – 2022 - Assignment 3
+// Program Name: xxxxxx.cpp
+// Last Modification Date: 07/04/2022
+// Author1 and ID and Group: 20210031
+// Author2 and ID and Group: 20210052
+// Author3 and ID and Group: 20210056
+// Teaching Assistant: Eng/ Hagar
+// Purpose: For 12 filters in photoshop project
 #include <iostream>
 #include "bmplib.cpp"
 #include <cmath>
